5eeb8f0936140756959dc976d4baee3483e2cb8d
miro1984 <sergey.makaruk@gmail.com>
2023-06-29 22:10:15 +0300

    calculator

---
+++ js/slider-calculator.js
@@ -0,0 +1,19 @@
+var swiperPaginatio = new Swiper(".calculator-slider-pagination", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+var swiperMain = new Swiper(".calculator-slider-main", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+        swiper: swiperPaginatio,
+    },
+});
---
+++ js/slider-product.js
@@ -0,0 +1,18 @@
+//     Slider Product
+    var swiper = new Swiper(".mySwiper", {
+		spaceBetween: 48,
+		loop: true,
+        hashNavigation: {
+        watchState: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+	  }); 
+
+
